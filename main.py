294b46c646efe0946485cd4ca4b5c8068d5c9529
CarparkingDizz <galabroten55@gmail.com>
2025-04-20 17:34:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from carparkingdizz import CarParkingDizz
+from carparktool import CARParkTool
 
 __CHANNEL_USERNAME__ = "CarParkingDizz_Tool"
 __GROUP_USERNAME__   = "Carparkingdizz_chat"
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CarParkingDizz(acc_access_key)
+        cpm = CarParkTool(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
